d0233ea8f6d95e2b5c48a5c9026235774f658772
Courage Zeng <czeng@lcs.on.ca>
2025-01-14 12:12:43 -0500

    Builded the static interface.

--- FindTheRoots/Views/RootsCalculatorView.swift
+++ FindTheRoots/Views/RootsCalculatorView.swift
@@ -8,20 +8,59 @@
 import SwiftUI
 
 struct RootsCalculatorView: View {
-    var body: some View {
+    @State private var a: Double = 1.0
+    @State private var b: Double = -6.0
+    @State private var c: Double = 8.0
         
+    var body: some View {
         NavigationStack {
             VStack {
                 Image("quadratic")
                     .resizable()
                     .frame(width: 300, height: 220)
                     .navigationTitle("Find the Roots")
-                Spacer()
                 
+                // Display the coefficient values
+                HStack {
+                    VStack {
+                        Text("a: \(a.formatted(.number.precision(.fractionLength(1))))")
+                            .font(Font.custom("Times New Roman",
+                                              size: 24.0,
+                                              relativeTo: .body))
+                            .padding()
+                        Slider(value: $a, in: -100...100, step: 1)
+                    }
+                    VStack {
+                        Text("b: \(b.formatted(.number.precision(.fractionLength(1))))")
+                            .font(Font.custom("Times New Roman",
+                                              size: 24.0,
+                                              relativeTo: .body))
+                            .padding()
+                        Slider(value: $b, in: -100...100, step: 1)
+                    }
+                    VStack {
+                        Text("c: \(c.formatted(.number.precision(.fractionLength(1))))")
+                            .font(Font.custom("Times New Roman",
+                                              size: 24.0,
+                                              relativeTo: .body))
+                            .padding()
+                        Slider(value: $c, in: -100...100, step: 1)
+                    }
+                }
+                
+                Text("x ≈ 2.00 and x ≈ 4.00")
+                    .font(Font.custom("Times New Roman",
+                                      size: 24.0,
+                                      relativeTo: .body))
+                
+                Spacer()
             }
+            .padding()
         }
     }
 }
+
+
 
 #Preview {
     RootsCalculatorView()
